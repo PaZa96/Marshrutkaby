6eca52a21f538d9d760a627f7175ea3f04ee3308
Pavel Zhavoronok <pahazavoronok@gmail.com>
2016-12-02 19:41:50 +0300

    Add project files.

---
+++ Marshrutkaby/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Marshrutkaby.Startup))]
+namespace Marshrutkaby
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
